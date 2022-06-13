7ab0c92521f62017f4a78b6f8b9642479ffd1bdc
TeiganPenna <teigan.penna@gmail.com>
2022-06-13 20:14:08 +1000

    stop using eval

--- maxhp-d20.user.js
+++ maxhp-d20.user.js
@@ -15,16 +15,44 @@
 (function() {
     'use strict';
 
+    function isNumber(n) {
+        return !isNaN(n);
+    }
+
     // replace 'd' with '*' and eval
     function calculate(breakdown) {
-        const equation = breakdown.replaceAll('d', '*');
-        return eval(equation);
+        // convert ndash and mdash to hyphen for convenience
+        breakdown = breakdown.replaceAll('–', '-').replaceAll('—', '-');
+
+        const termsToAdd = breakdown.split('+');
+
+        let sum = 0;
+        for (const term of termsToAdd) {
+            if (isNumber(term)) {
+                sum += parseInt(term);
+                continue;
+            }
+
+            // handle negative modifier (including mdash and ndash
+            const [hitDice, negMod] = term.split('-');
+            if (isNumber(negMod)) {
+                sum -= negMod;
+            }
+
+            const [totalHitDice, hitDie] = hitDice.split('d');
+            if (isNumber(totalHitDice) && isNumber(hitDie)) {
+                const maxHpFromHitDice = parseInt(hitDice) * parseInt(hitDie);
+                sum += maxHpFromHitDice;
+            }
+        }
+
+        return sum;
     }
 
     $(document).ready(() => {
         const statblock = $('.article-content');
         if (statblock.length) {
-            const hpBreakdownRegex = /<b>hp<\/b>\s\d+\s\(([0-9d\+\-]+)\)/;
+            const hpBreakdownRegex = /<b>hp<\/b>\s\d+\s\(([0-9d\+\-\–\—]+)\)/;
             const statblockHtml = statblock.html();
             const breakdownMatch = statblockHtml.match(hpBreakdownRegex);
 
